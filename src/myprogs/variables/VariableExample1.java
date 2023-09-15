e1640839799a2c6b0552fb16d1811c68db142f8e
Saifuddin05 <saifuddin@skillbridge.tech>
2023-09-15 18:07:54 +0530

    another commit

---
+++ src/myprogs/variables/VariableExample1.java
@@ -0,0 +1,22 @@
+package myprogs.variables;
+
+public class VariableExample1 {
+
+    public static void main(String[] args) {
+        //declare and initialize 2 int data type variables
+        //add those 2 int variables and store in another int variable
+        //if that stored value greater than 50 then print "i am greater than 50"
+        //else print "i am not greater than 50"
+        int a = 21;
+        int b = 23;
+        int c = a+b;
+         if (c<50){
+             System.out.println("i am greater than 50");
+         }
+         else {
+             System.out.println("i am not greater than 50");
+         }
+
+
+    }
+}
