c09708d39b436b37b9d8031bc9e42ce81efcc2df
Saifuddin05 <saifuddin@skillbridge.tech>
2023-09-19 10:49:20 +0530

    another commit

--- src/myprogs/variables/VariableExample1.java
+++ src/myprogs/variables/VariableExample1.java
@@ -9,13 +9,12 @@ public class VariableExample1 {
         //else print "i am not greater than 50"
         int a = 21;
         int b = 23;
-        int c = a+b;
-         if (c<50){
-             System.out.println("i am greater than 50");
-         }
-         else {
-             System.out.println("i am not greater than 50");
-         }
+        int c = a + b;
+        if (c > 50) {
+            System.out.println("i am greater than 50");
+        } else {
+            System.out.println("i am not greater than 50");
+        }
 
 
     }
