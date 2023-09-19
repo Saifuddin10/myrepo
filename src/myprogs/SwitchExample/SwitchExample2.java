cb7e991c9baf6cacc6651befd8034c1a145fd083
Saifuddin05 <saifuddin@skillbridge.tech>
2023-09-19 15:23:09 +0530

    SwitchExample2 commit

---
+++ src/myprogs/SwitchExample/SwitchExample2.java
@@ -0,0 +1,26 @@
+package myprogs.SwitchExample;
+
+public class SwitchExample2 {
+    public static void main(String[] args) {
+        //tow variables,add and store in another variable
+        //check that variable equals to 3 ,5, 7 using switch
+        //default message -> does not belong to 3,5,7
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        switch (c) {
+            case 3:
+                System.out.println("The result is equal to 3");
+                break;
+            case 5:
+                System.out.println("The result is equal to 5");
+                break;
+            case 7:
+                System.out.println("The result is equal to 7");
+                break;
+            default:
+                System.out.println("the result is not equal to 3,5,7");
+
+        }
+    }
+}
