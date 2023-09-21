e2ebb1e2f4f56f04be0c46dd293b906d3eb35960
Saifuddin05 <saifuddin@skillbridge.tech>
2023-09-21 15:15:16 +0530

    Loops Example2 commit

--- src/myprogs/Loops/LoopsExample1.java
+++ src/myprogs/Loops/LoopsExample1.java
@@ -4,13 +4,12 @@ public class LoopsExample1 {
     public static void main(String[] args) {
         int i;
         for (i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
 
+                System.out.println("* ");
+            }
+            System.out.println();
         }
-        for (int j = 1; j <= i; j++) {
-
-            System.out.println("* ");
-        }
-        System.out.println();
     }
-
 }
+
