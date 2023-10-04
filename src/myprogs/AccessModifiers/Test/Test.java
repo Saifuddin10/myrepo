d17d183cded05cd0a999b2d4bd1df7a127145d10
Saifuddin05 <saifuddin@skillbridge.tech>
2023-10-04 18:28:15 +0530

    Test Oops commit

---
+++ src/myprogs/AccessModifiers/Test/Test.java
@@ -0,0 +1,11 @@
+package myprogs.AccessModifiers.Test;
+
+import myprogs.AccessModifiers.Demo;
+
+public class Test {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        // By calling defaul access modifier
+//        obj.show();
+    }
+}
