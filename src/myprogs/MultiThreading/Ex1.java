baf4faf2e32e5b0b0bad5c11bc5cfbce7710671a
Saifuddin05 <saifuddin@skillbridge.tech>
2023-12-11 10:01:09 +0530

    MultiThreading commit

---
+++ src/myprogs/MultiThreading/Ex1.java
@@ -0,0 +1,9 @@
+package myprogs.MultiThreading;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        t.setName("Saif");
+        System.out.println("Thread name is : " + t);
+    }
+}
